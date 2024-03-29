e8b130f0af241eb603153cff413fda1bf5223059
Massimo Melina <a@rejetto.com>
2024-03-29 18:56:29 +0100

    1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Windows URL files
+
+HFS plugin to redirect the browser when a `.url` file is open. URL files are created by Windows.
--- dist/plugin.js
+++ dist/plugin.js
@@ -1,9 +1,19 @@
 exports.version = 1
-exports.description = "This is an empty plugin, for testing purposes"
+exports.description = "Windows URL files (redirect on open)"
 exports.apiRequired = 1
-exports.repo = "rejetto/demo-plugin"
-exports.depend = [{ "repo": "rejetto/thumbnails" }]
+exports.repo = "rejetto/windows-url-files"
 
-exports.init = api => {
-    api.log("hi!")
-}
+exports.init = api => ({
+    middleware: ctx => async () => { // wait for it to be processed, so that permissions and the rest is taken care of
+        if ('dl' in ctx.query) return // forced download
+        const file = ctx.state.fileSource
+            || ctx.fileSource // legacy
+        if (!file?.endsWith('.url')) return
+        const { readFile } = api.require('fs/promises')
+        const content = await readFile(file)
+        if (!content.includes('[InternetShortcut]')) return
+        const url = /^URL=(.+)$/m.exec(content)?.[1]
+        if (!url) return
+        ctx.redirect(url)
+    }
+})
